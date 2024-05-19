b2cb2851007c53b3b76c628a9df35cf84542c1ca
SaniBorek <borek@gmail.com>
2024-05-19 16:19:42 +0300

    h

--- src/Lending/lendingProtocols.sol
+++ src/Lending/lendingProtocols.sol
@@ -7,7 +7,8 @@ import "@hack/Lending/mathematics.sol";
 contract LedingProtocals is ERC20 ,Mathematics{
 
     address owner;
-    mapping (address => uint) public lenders; 
+    mapping (address => uint) public lendersDai; 
+     mapping (address => uint) public lendersEth; 
     ERC20 DAI;
     ERC20 bond;
     uint maxLTV = 80;
@@ -25,26 +26,43 @@ contract LedingProtocals is ERC20 ,Mathematics{
     function lendingDAI(uint amount) public validAmount(amount){
 
         DAI.transferFrom(msg.sender, address(this), amount);
-        lenders[msg.sender] += amount;
+        lendersDai[msg.sender] += amount;
         collateralValue += amount;
         _mint(msg.sender, amount);
 
     }
 
     function receiveTheLendingDAY(uint amount) external validAmount(amount){
-        require(lenders[msg.sender] >= amount, "You did not deposit enough DAI");
+        require(lendersDai[msg.sender] >= amount, "You did not deposit enough DAI");
         require(bond.balanceOf(msg.sender) >= amount, "You dont have enough bond");
         _burn(msg.sender, amount);
         collateralValue -= amount;
         DAI.transfer(msg.sender, amount);
-        lenders[msg.sender] -= amount;
+        lendersDai[msg.sender] -= amount;
 
     }
     function borrow(uint borrowedValue) external validAmount(borrowedValue) payable{
         require(msg.value <= msg.sender.balance, "you dont have enough eth");
         uint borrowLimit = percentage((collateralValue - borrowedValue), maxLTV);
+        lendersEth[msg.sender] = msg.value;
         require(borrowLimit <= borrowedValue, "You have a limeted borrow ");
+        DAI.transferFrom(address(this), msg.sender, borrowedValue);
 
+    }
+
+    function removeETH(uint amount ) external validAmount(amount){
+        require( amount * 3,116 <= lendersDai[msg.sender], "you dont have enough dai");
+        payable(address(msg.sender)).transfer(amount);
+        lendersDai[msg.sender] -= amount * 3,116 ;
+        collateralValue -= amount * 3,116 ;
+
+    }
+
+    function borrowDAI(uint amount) external validAmount(amount){
+        require(amount/3,116 >= lendingEth[msg.sender], "you dont have enough eth");
+        dai.transferFrom(address(this), msg.sender, amount/3116);
+        lendersEth[msg.sender]  -= amount/3116;
+        
     }
 
 }
