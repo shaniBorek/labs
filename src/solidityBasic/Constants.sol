2a54d8a0a2c4b49656e547388d9ec197bbb8838b
shaniBorek <shaniborek@gmail.com>
2024-06-03 23:59:32 +0300

    UPDATE

---
+++ src/solidityBasic/Constants.sol
@@ -0,0 +1,7 @@
+//SPDEX-Licens-Identifier: MIT
+pragma solidity ^0.8.19;
+contract Constants {
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
